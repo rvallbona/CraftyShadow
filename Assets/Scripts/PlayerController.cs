e8d863d08a2efc2fcb3952ca388bf5e243629556
Ramon Vallbona Degollada <rvallbona2001@gmail.com>
2024-03-25 02:45:49 +0100

    First try change movement rigitbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,8 +33,8 @@ public class PlayerController : MonoBehaviour
     {
         Inputs();
         Movement();
-        LightLogic();
         characterController.Move(playerVelocity * Time.deltaTime);
+        LightLogic();
     }
     private void FixedUpdate()
     {
